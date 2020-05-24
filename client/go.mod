6375c9d1a23d0e90a13dae80684d077181e9488d
RenzHoly <renzholy@gmail.com>
2020-05-24 18:45:54 +0800

    add client

---
+++ client/go.mod
@@ -0,0 +1,9 @@
+module github.com/renzholy/mongood/server
+
+go 1.14
+
+require (
+	github.com/gobuffalo/packr v1.30.1
+	github.com/zserge/webview v0.0.0-20200516134046-4caf698fad53
+	go.mongodb.org/mongo-driver v1.3.3
+)
---
+++ client/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/gobuffalo/packr"
+	"github.com/zserge/webview"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	ctx      context.Context
+	database *mongo.Database
+)
+
+func main() {
+	url := os.Getenv("MONGO_URL")
+	if url == "" {
+		url = "mongodb://localhost:27017"
+	}
+	root := os.Getenv("ROOT")
+	if root == "" {
+		root = "../dist"
+	}
+	client, _ := mongo.NewClient(options.Client().ApplyURI(url))
+	database = client.Database("push")
+	ctx = context.Background()
+	client.Connect(ctx)
+	defer client.Disconnect(ctx)
+	folder := packr.NewBox(root)
+	http.Handle("/", http.FileServer(folder))
+	http.HandleFunc("/api/runCommand", func(w http.ResponseWriter, r *http.Request) {
+		type Request struct {
+			Database string
+			Command  string
+		}
+		request := Request{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		var command interface{}
+		err = bson.UnmarshalExtJSON([]byte(request.Command), true, &command)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		raw, err := client.Database(request.Database).RunCommand(ctx, command).DecodeBytes()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		var str map[string]interface{}
+		bson.Unmarshal(raw, &str)
+		json, err := json.Marshal(str)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/bson")
+		w.Write(json)
+	})
+	go http.ListenAndServe(":3000", nil)
+	w := webview.New(true)
+	defer w.Destroy()
+	w.SetSize(1280, 800, webview.HintNone)
+	w.Navigate("http://localhost:3000")
+	w.SetTitle("Mongood")
+	w.Run()
+}
