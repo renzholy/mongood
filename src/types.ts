cd18d8710c4fb157374e12e4e6e71601b4e0fcd8
Zehua Ren <renzholy@gmail.com>
2020-05-26 00:24:38 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -35,4 +35,75 @@ export type StatDetail = {
   uri: string
 }
 
-export type JsonSchema = {}
+type BaseJsonSchema = {
+  title?: string
+  description?: string
+  allOf?: JsonSchema[]
+  anyOf?: JsonSchema[]
+  oneOf?: JsonSchema[]
+  not?: JsonSchema
+}
+
+type ObjectJsonSchema = {
+  bsonType: 'object'
+  type: 'object'
+  properties: { [key: string]: JsonSchema }
+  enum?: object[]
+  maxProperties?: number
+  minProperties?: number
+  required?: string[]
+  additionalProperties?: boolean | JsonSchema
+  patternProperties?: { [regex: string]: JsonSchema }
+  dependencies?: object
+}
+
+type ArrayJsonSchema = {
+  bsonType: 'array'
+  type: 'array'
+  items: JsonSchema | JsonSchema[]
+  enum?: [][]
+  additionalItems?: boolean | JsonSchema
+  maxItems?: number
+  minItems?: number
+  uniqueItems?: number
+}
+
+type NumberJsonSchema = {
+  bsonType: 'int' | 'long'
+  type: 'number'
+  enum?: number[]
+  multipleOf?: number
+  maximum?: number
+  exclusiveMaximum?: number
+  minimum?: number
+  exclusiveMinimum?: number
+}
+
+type StringJsonSchema = {
+  bsonType: 'string'
+  type: 'string'
+  enum?: string[]
+  maxLength?: number
+  minLength?: number
+  pattern?: string
+}
+
+type BooleanJsonSchema = {
+  bsonType: 'boolean'
+  type: 'boolean'
+}
+
+type NullJsonSchema = {
+  bsonType: 'null'
+  type: 'null'
+}
+
+export type JsonSchema = (
+  | ObjectJsonSchema
+  | ArrayJsonSchema
+  | NumberJsonSchema
+  | StringJsonSchema
+  | BooleanJsonSchema
+  | NullJsonSchema
+) &
+  BaseJsonSchema
