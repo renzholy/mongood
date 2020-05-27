6c3a39e84d553e52a5147f93f70984c878f11ee7
Zehua Ren <renzholy@gmail.com>
2020-05-27 20:04:02 +0800

    Update main.go

--- client/main.go
+++ client/main.go
@@ -17,9 +17,35 @@ import (
 )
 
 var (
-	ctx      context.Context
-	database *mongo.Database
+	ctx    context.Context
+	client *mongo.Client
 )
+
+func runCommand(w http.ResponseWriter, r *http.Request) {
+	type Request struct {
+		Database string
+		Command  string
+	}
+	request := Request{}
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var command interface{}
+	err = bson.UnmarshalExtJSON([]byte(request.Command), true, &command)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	raw, err := client.Database(request.Database).RunCommand(ctx, command).DecodeBytes()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(raw.String()))
+}
 
 func main() {
 	port, err := freeport.GetFreePort()
@@ -35,38 +61,17 @@ func main() {
 	if root == "" {
 		root = "../dist"
 	}
-	client, _ := mongo.NewClient(options.Client().ApplyURI(url))
-	database = client.Database("push")
+	client, err = mongo.NewClient(options.Client().ApplyURI(url))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	ctx = context.Background()
 	client.Connect(ctx)
 	defer client.Disconnect(ctx)
 	folder := packr.NewBox(root)
 	http.Handle("/", http.FileServer(folder))
-	http.HandleFunc("/api/runCommand", func(w http.ResponseWriter, r *http.Request) {
-		type Request struct {
-			Database string
-			Command  string
-		}
-		request := Request{}
-		err := json.NewDecoder(r.Body).Decode(&request)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		var command interface{}
-		err = bson.UnmarshalExtJSON([]byte(request.Command), true, &command)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		raw, err := client.Database(request.Database).RunCommand(ctx, command).DecodeBytes()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(raw.String()))
-	})
+	http.HandleFunc("/api/runCommand", runCommand)
 	go http.ListenAndServe(":"+strconv.Itoa(port), nil)
 	w := webview.NewWebview(webview.Settings{
 		Title:     "Mongood",
