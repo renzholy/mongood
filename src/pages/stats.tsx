afe30987bbc54f6cbb6b6506b4ff7cbe20b025ba
RenzHoly <renzholy@gmail.com>
2020-06-03 01:58:05 +0800

    Update stats.tsx

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -6,6 +6,7 @@ import { Card } from '@uifabric/react-cards'
 import bytes from 'bytes'
 import prettyMilliseconds from 'pretty-ms'
 import { CollStats } from 'mongodb'
+import _ from 'lodash'
 
 import { runCommand } from '@/utils/fetcher'
 import { Number } from '@/utils/formatter'
@@ -45,8 +46,47 @@ function InfoCard(props: { title: string; content: string }) {
   )
 }
 
-export default () => {
+function InfoArea(props: {
+  title: string
+  subtitle?: string
+  data?: { [title: string]: string }
+}) {
   const theme = getTheme()
+
+  return (
+    <>
+      <Text
+        variant="xxLarge"
+        block={true}
+        styles={{
+          root: { padding: 10, color: theme.palette.neutralPrimary },
+        }}>
+        {props.title}
+        <span style={{ color: theme.palette.neutralSecondary }}>
+          {props.subtitle}
+        </span>
+      </Text>
+      {_.isEmpty(props.data)
+        ? null
+        : _.chunk(
+            _.map(props.data, (content, title) => ({ content, title })),
+            3,
+          ).map((data, index) => (
+            <Stack
+              key={index.toString()}
+              tokens={{ padding: 10, childrenGap: 20 }}
+              horizontal={true}
+              styles={{ root: { overflowX: 'scroll' } }}>
+              {data.map(({ content, title }) => (
+                <InfoCard key={title} title={`${title}:`} content={content} />
+              ))}
+            </Stack>
+          ))}
+    </>
+  )
+}
+
+export default () => {
   const { database, collection } = useSelector((state) => state.root)
   const { data: collStats } = useSWR(
     database && collection ? `collStats/${database}/${collection}` : null,
@@ -123,200 +163,73 @@ export default () => {
     if (serverStatus) {
       return (
         <div style={{ overflowY: 'scroll', padding: 10, margin: '0 auto' }}>
-          <Text
-            variant="xxLarge"
-            block={true}
-            styles={{
-              root: { padding: 10, color: theme.palette.neutralPrimary },
-            }}>
-            <span style={{ color: theme.palette.neutralSecondary }}>
-              Host:&nbsp;
-            </span>
-            {serverStatus.host}
-          </Text>
-          <Text
-            variant="xxLarge"
-            block={true}
-            styles={{
-              root: { padding: 10, color: theme.palette.neutralPrimary },
-            }}>
-            <span style={{ color: theme.palette.neutralSecondary }}>
-              Uptime:&nbsp;
-            </span>
-            {prettyMilliseconds(serverStatus.uptimeMillis)}
-          </Text>
+          <InfoArea title="Host: " subtitle={serverStatus.host} />
+          <InfoArea
+            title="Uptime: "
+            subtitle={prettyMilliseconds(serverStatus.uptimeMillis)}
+          />
           {serverStatus.repl ? (
-            <>
-              <Text
-                variant="xxLarge"
-                block={true}
-                styles={{
-                  root: { padding: 10, color: theme.palette.neutralPrimary },
-                }}>
-                <span style={{ color: theme.palette.neutralSecondary }}>
-                  Repl:&nbsp;
-                </span>
-                {serverStatus.repl.setName}
-              </Text>
-              <Stack
-                tokens={{ padding: 10, childrenGap: 20 }}
-                horizontal={true}
-                styles={{ root: { overflowX: 'scroll' } }}>
-                {serverStatus.repl.hosts.map((host) => (
-                  <InfoCard
-                    key={host}
-                    title={
-                      host === serverStatus.repl!.primary
-                        ? 'Primary:'
-                        : 'Secondary:'
-                    }
-                    content={host}
-                  />
-                ))}
-              </Stack>
-            </>
+            <InfoArea
+              title="Repl: "
+              subtitle={serverStatus.repl.setName}
+              data={serverStatus.repl.hosts.reduce((prev, curr, index) => {
+                // eslint-disable-next-line no-param-reassign
+                prev[
+                  `${
+                    curr === serverStatus.repl!.primary
+                      ? 'Primary'
+                      : 'Secondary'
+                  } ${index}`
+                ] = curr
+                return prev
+              }, {} as { [key: string]: string })}
+            />
           ) : null}
-          <Text
-            variant="xxLarge"
-            block={true}
-            styles={{
-              root: { padding: 10, color: theme.palette.neutralPrimary },
-            }}>
-            Connections
-          </Text>
-          <Stack
-            tokens={{ padding: 10, childrenGap: 20 }}
-            horizontal={true}
-            styles={{ root: { overflowX: 'scroll' } }}>
-            <InfoCard
-              title="Available:"
-              content={Number.format(serverStatus.connections.available)}
-            />
-            <InfoCard
-              title="Current:"
-              content={Number.format(serverStatus.connections.current)}
-            />
-            <InfoCard
-              title="Total Created:"
-              content={Number.format(serverStatus.connections.totalCreated)}
-            />
-          </Stack>
-          <Text
-            variant="xxLarge"
-            block={true}
-            styles={{
-              root: { padding: 10, color: theme.palette.neutralPrimary },
-            }}>
-            Network
-          </Text>
-          <Stack
-            tokens={{ padding: 10, childrenGap: 20 }}
-            horizontal={true}
-            styles={{ root: { overflowX: 'scroll' } }}>
-            <InfoCard
-              title="In:"
-              content={bytes(serverStatus.network.bytesIn, {
+          <InfoArea
+            title="Connections"
+            data={{
+              Available: Number.format(serverStatus.connections.available),
+              Current: Number.format(serverStatus.connections.current),
+              'Total Created': Number.format(
+                serverStatus.connections.totalCreated,
+              ),
+            }}
+          />
+          <InfoArea
+            title="Network"
+            data={{
+              In: bytes(serverStatus.network.bytesIn, {
                 unitSeparator: ' ',
-              })}
-            />
-            <InfoCard
-              title="Out:"
-              content={bytes(serverStatus.network.bytesOut, {
+              }),
+              Out: bytes(serverStatus.network.bytesOut, {
                 unitSeparator: ' ',
-              })}
-            />
-            <InfoCard
-              title="Requests:"
-              content={Number.format(serverStatus.network.numRequests)}
-            />
-          </Stack>
-          <Text
-            variant="xxLarge"
-            block={true}
-            styles={{
-              root: { padding: 10, color: theme.palette.neutralPrimary },
-            }}>
-            Op Counters
-          </Text>
-          <Stack
-            tokens={{ padding: 10, childrenGap: 20 }}
-            horizontal={true}
-            styles={{ root: { overflowX: 'scroll' } }}>
-            <InfoCard
-              title="Insert:"
-              content={Number.format(serverStatus.opcounters.insert)}
-            />
-            <InfoCard
-              title="Query:"
-              content={Number.format(serverStatus.opcounters.query)}
-            />
-            <InfoCard
-              title="Update:"
-              content={Number.format(serverStatus.opcounters.update)}
-            />
-          </Stack>
-          <Stack
-            tokens={{ padding: 10, childrenGap: 20 }}
-            horizontal={true}
-            styles={{ root: { overflowX: 'scroll' } }}>
-            <InfoCard
-              title="Delete:"
-              content={Number.format(serverStatus.opcounters.delete)}
-            />
-            <InfoCard
-              title="Get More:"
-              content={Number.format(serverStatus.opcounters.getmore)}
-            />
-            <InfoCard
-              title="Command:"
-              content={Number.format(serverStatus.opcounters.command)}
-            />
-          </Stack>
+              }),
+              Requests: Number.format(serverStatus.network.numRequests),
+            }}
+          />
+          <InfoArea
+            title="Op Counters"
+            data={{
+              Insert: Number.format(serverStatus.opcounters.insert),
+              Query: Number.format(serverStatus.opcounters.query),
+              Update: Number.format(serverStatus.opcounters.update),
+              Delete: Number.format(serverStatus.opcounters.delete),
+              'Get More': Number.format(serverStatus.opcounters.getmore),
+              Command: Number.format(serverStatus.opcounters.command),
+            }}
+          />
           {serverStatus.opcountersRepl ? (
-            <>
-              <Text
-                variant="xxLarge"
-                block={true}
-                styles={{
-                  root: { padding: 10, color: theme.palette.neutralPrimary },
-                }}>
-                Op Counters Repl
-              </Text>
-              <Stack
-                tokens={{ padding: 10, childrenGap: 20 }}
-                horizontal={true}
-                styles={{ root: { overflowX: 'scroll' } }}>
-                <InfoCard
-                  title="Insert:"
-                  content={Number.format(serverStatus.opcountersRepl.insert)}
-                />
-                <InfoCard
-                  title="Query:"
-                  content={Number.format(serverStatus.opcountersRepl.query)}
-                />
-                <InfoCard
-                  title="Update:"
-                  content={Number.format(serverStatus.opcountersRepl.update)}
-                />
-              </Stack>
-              <Stack
-                tokens={{ padding: 10, childrenGap: 20 }}
-                horizontal={true}
-                styles={{ root: { overflowX: 'scroll' } }}>
-                <InfoCard
-                  title="Delete:"
-                  content={Number.format(serverStatus.opcountersRepl.delete)}
-                />
-                <InfoCard
-                  title="Get More:"
-                  content={Number.format(serverStatus.opcountersRepl.getmore)}
-                />
-                <InfoCard
-                  title="Command:"
-                  content={Number.format(serverStatus.opcountersRepl.command)}
-                />
-              </Stack>
-            </>
+            <InfoArea
+              title="Op Counters Repl"
+              data={{
+                Insert: Number.format(serverStatus.opcountersRepl.insert),
+                Query: Number.format(serverStatus.opcountersRepl.query),
+                Update: Number.format(serverStatus.opcountersRepl.update),
+                Delete: Number.format(serverStatus.opcountersRepl.delete),
+                'Get More': Number.format(serverStatus.opcountersRepl.getmore),
+                Command: Number.format(serverStatus.opcountersRepl.command),
+              }}
+            />
           ) : null}
         </div>
       )
@@ -325,97 +238,43 @@ export default () => {
   }
   return (
     <div style={{ overflowY: 'scroll', padding: 10, margin: '0 auto' }}>
-      <Text
-        variant="xxLarge"
-        block={true}
-        styles={{ root: { padding: 10, color: theme.palette.neutralPrimary } }}>
-        <span style={{ color: theme.palette.neutralSecondary }}>
-          Database:&nbsp;
-        </span>
-        {dbStats.db}
-      </Text>
-      <Stack
-        tokens={{ padding: 10, childrenGap: 20 }}
-        horizontal={true}
-        styles={{ root: { overflowX: 'scroll' } }}>
-        <InfoCard
-          title="Size:"
-          content={bytes(dbStats.dataSize, { unitSeparator: ' ' })}
-        />
-        <InfoCard
-          title="Index Size:"
-          content={bytes(dbStats.indexSize, { unitSeparator: ' ' })}
-        />
-        <InfoCard
-          title="Storage Size:"
-          content={bytes(dbStats.storageSize, { unitSeparator: ' ' })}
-        />
-      </Stack>
-      <Stack
-        tokens={{ padding: 10, childrenGap: 20 }}
-        horizontal={true}
-        styles={{ root: { overflowX: 'scroll' } }}>
-        <InfoCard title="Count:" content={Number.format(dbStats.objects)} />
-        <InfoCard
-          title="Average Object Size:"
-          content={bytes(dbStats.avgObjSize || 0, { unitSeparator: ' ' })}
-        />
-        <InfoCard
-          title="Collections + Views:"
-          content={Number.format(dbStats.collections + dbStats.views)}
-        />
-      </Stack>
-      <Stack
-        tokens={{ padding: 10, childrenGap: 20 }}
-        horizontal={true}
-        styles={{ root: { overflowX: 'scroll' } }}>
-        <InfoCard title="Indexes:" content={Number.format(dbStats.indexes)} />
-        <InfoCard
-          title="FS Used Size:"
-          content={bytes(dbStats.fsUsedSize || 0, { unitSeparator: ' ' })}
-        />
-        <InfoCard
-          title="FS Total Size:"
-          content={bytes(dbStats.fsTotalSize || 0, { unitSeparator: ' ' })}
-        />
-      </Stack>
-      <Text
-        variant="xxLarge"
-        block={true}
-        styles={{ root: { padding: 10, color: theme.palette.neutralPrimary } }}>
-        <span style={{ color: theme.palette.neutralSecondary }}>
-          Collection:&nbsp;
-        </span>
-        {collection}
-      </Text>
-      <Stack
-        tokens={{ padding: 10, childrenGap: 20 }}
-        horizontal={true}
-        styles={{ root: { overflowX: 'scroll' } }}>
-        <InfoCard
-          title="Size:"
-          content={bytes(collStats.size, { unitSeparator: ' ' })}
-        />
-        <InfoCard
-          title="Index Size:"
-          content={bytes(collStats.totalIndexSize, { unitSeparator: ' ' })}
-        />
-        <InfoCard
-          title="Storage Size:"
-          content={bytes(collStats.storageSize, { unitSeparator: ' ' })}
-        />
-      </Stack>
-      <Stack
-        tokens={{ padding: 10, childrenGap: 20 }}
-        horizontal={true}
-        styles={{ root: { overflowX: 'scroll' } }}>
-        <InfoCard title="Count:" content={Number.format(collStats.count)} />
-        <InfoCard
-          title="Average Object Size:"
-          content={bytes(collStats.avgObjSize || 0, { unitSeparator: ' ' })}
-        />
-        <InfoCard title="Capped:" content={collStats.capped ? 'Yes' : 'No'} />
-      </Stack>
+      <InfoArea
+        title="Database: "
+        subtitle={dbStats.db}
+        data={{
+          Size: bytes(dbStats.dataSize, { unitSeparator: ' ' }),
+          'Index Size': bytes(dbStats.indexSize, { unitSeparator: ' ' }),
+          'Storage Size': bytes(dbStats.storageSize, { unitSeparator: ' ' }),
+          Count: Number.format(dbStats.objects),
+          'Average Object Size': bytes(dbStats.avgObjSize || 0, {
+            unitSeparator: ' ',
+          }),
+          'Collections + Views': Number.format(
+            dbStats.collections + dbStats.views,
+          ),
+          Indexes: Number.format(dbStats.indexes),
+          'FS Used Size': bytes(dbStats.fsUsedSize || 0, {
+            unitSeparator: ' ',
+          }),
+          'FS Total Size': bytes(dbStats.fsTotalSize || 0, {
+            unitSeparator: ' ',
+          }),
+        }}
+      />
+      <InfoArea
+        title="Collection: "
+        subtitle={collection}
+        data={{
+          Size: bytes(collStats.size, { unitSeparator: ' ' }),
+          'Index Size': bytes(collStats.totalIndexSize, { unitSeparator: ' ' }),
+          'Storage Size': bytes(collStats.storageSize, { unitSeparator: ' ' }),
+          Count: Number.format(collStats.count),
+          'Average Object Size': bytes(collStats.avgObjSize || 0, {
+            unitSeparator: ' ',
+          }),
+          Capped: collStats.capped ? 'Yes' : 'No',
+        }}
+      />
     </div>
   )
 }
