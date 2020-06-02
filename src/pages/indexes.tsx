60ff0478b2b12f0992c27a810bd2d012b1a88a85
RenzHoly <renzholy@gmail.com>
2020-06-03 02:02:10 +0800

    remove IndexCardList

--- src/pages/indexes.tsx
+++ src/pages/indexes.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import useSWR from 'swr'
-import { CollStats } from 'mongodb'
+import { CollStats, IndexSpecification } from 'mongodb'
 
-import { IndexCardList } from '@/components/IndexCardList'
 import { runCommand } from '@/utils/fetcher'
 import { LargeMessage } from '@/components/LargeMessage'
+import { Stack } from '@fluentui/react'
+import { IndexCard } from '@/components/IndexCard'
 
 export default () => {
   const { database, collection } = useSelector((state) => state.root)
@@ -18,16 +19,36 @@ export default () => {
     },
     { refreshInterval: 5 * 1000 },
   )
+  const { data: indexes } = useSWR(
+    database && collection ? `listIndexes/${database}/${collection}` : null,
+    () => {
+      return runCommand<{ cursor: { firstBatch: IndexSpecification[] } }>(
+        database!,
+        {
+          listIndexes: collection,
+        },
+      )
+    },
+  )
 
   if (!stats) {
     return <LargeMessage iconName="Back" title="Select collection" />
   }
+  if (!indexes?.cursor.firstBatch.length) {
+    return <LargeMessage iconName="Database" title="No Index" />
+  }
   return (
     <div style={{ overflowY: 'scroll', padding: 10, margin: '0 auto' }}>
-      <IndexCardList
-        indexDetails={stats.indexDetails}
-        indexSizes={stats.indexSizes}
-      />
+      <Stack tokens={{ childrenGap: 20, padding: 10 }}>
+        {indexes.cursor.firstBatch.map((item) => (
+          <IndexCard
+            key={item.name}
+            value={item}
+            size={stats.indexSizes[item.name!]}
+            statDetail={stats.indexDetails[item.name!]}
+          />
+        ))}
+      </Stack>
     </div>
   )
 }
