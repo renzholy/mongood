3965a96ce1ae1ef69af970747fb034d8b61b8079
Zehua Ren <renzholy@gmail.com>
2020-06-22 17:01:41 +0800

    Update Profiling (#19)
    * update deps
    
    * Update profiling.tsx
    
    * ExecStage
    
    * Update profiling.tsx
    
    * Update profiling.tsx
    
    * update ExecStage

--- src/components/SystemProfileCard.tsx
+++ src/components/SystemProfileCard.tsx
@@ -1,150 +1,14 @@
 /* eslint-disable react/no-danger */
 
-import React, { useEffect, useState, useMemo, useCallback } from 'react'
+import React, { useState } from 'react'
 import { Card } from '@uifabric/react-cards'
-import {
-  Text,
-  getTheme,
-  Icon,
-  CompoundButton,
-  HoverCard,
-  HoverCardType,
-} from '@fluentui/react'
+import { Text, getTheme } from '@fluentui/react'
 import _ from 'lodash'
-import bytes from 'bytes'
 
-import { SystemProfileDoc, ExecStats } from '@/types'
-import { stringify } from '@/utils/mongo-shell-data'
-import { colorize } from '@/utils/editor'
-import { useDarkMode } from '@/utils/theme'
+import { SystemProfileDoc } from '@/types'
 import { Number } from '@/utils/formatter'
 import { SystemProfileModal } from './SystemProfileModal'
-
-function ExecStage(props: { value: ExecStats }) {
-  const theme = getTheme()
-  const isDarkMode = useDarkMode()
-  const str = useMemo(
-    () => stringify(_.omit(props.value, ['inputStage', 'inputStages']), 2),
-    [props.value],
-  )
-  const [html, setHtml] = useState(str)
-  useEffect(() => {
-    colorize(str, isDarkMode).then(setHtml)
-  }, [str, isDarkMode])
-  const onRenderPlainCard = useCallback(() => {
-    return (
-      <div
-        style={{
-          paddingLeft: 10,
-          paddingRight: 10,
-          maxWidth: 500,
-          maxHeight: 500,
-          overflowY: 'scroll',
-          backgroundColor: theme.palette.neutralLighterAlt,
-        }}>
-        <pre
-          style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    )
-  }, [html])
-
-  return (
-    <>
-      <HoverCard
-        type={HoverCardType.plain}
-        plainCardProps={{ onRenderPlainCard }}
-        styles={{
-          host: {
-            display: 'inherit',
-            cursor: 'pointer',
-          },
-        }}
-        instantOpenOnClick={true}>
-        <CompoundButton
-          styles={{
-            description: {
-              whiteSpace: 'pre-wrap',
-              lineHeight: '1.2em',
-            },
-            root: {
-              paddingTop: 10,
-              paddingBottom: 10,
-              minHeight: 'unset',
-              height: 'fit-content',
-            },
-          }}
-          secondaryText={_.compact([
-            `${Number.format(
-              props.value.executionTimeMillisEstimate -
-                (props.value.inputStage?.executionTimeMillisEstimate || 0),
-            )} ms`,
-            props.value.docsExamined === undefined
-              ? undefined
-              : `${Number.format(props.value.docsExamined)} docs examined`,
-            props.value.keysExamined === undefined
-              ? undefined
-              : `${Number.format(props.value.keysExamined)} keys examined`,
-            props.value.nMatched === undefined
-              ? undefined
-              : `${Number.format(props.value.nMatched)} matched`,
-            props.value.memUsage === undefined
-              ? undefined
-              : `${bytes(props.value.memUsage, {
-                  unitSeparator: ' ',
-                }).toLocaleLowerCase()} mem used`,
-            `${Number.format(props.value.nReturned)} returned`,
-          ]).join('\n')}>
-          {props.value.stage}
-        </CompoundButton>
-      </HoverCard>
-      {props.value.inputStage ? (
-        <>
-          <Icon
-            iconName="Forward"
-            styles={{
-              root: {
-                color: theme.palette.neutralPrimary,
-                margin: 8,
-              },
-            }}
-          />
-          <ExecStage value={props.value.inputStage} />
-        </>
-      ) : null}
-      {props.value.inputStages ? (
-        <>
-          <Icon
-            iconName="Forward"
-            styles={{
-              root: {
-                color: theme.palette.neutralPrimary,
-                margin: 8,
-              },
-            }}
-          />
-          {props.value.inputStages.map((inputStage, index) => (
-            <>
-              {index === 0 ? null : (
-                <Icon
-                  iconName="Pause"
-                  styles={{
-                    root: {
-                      color: theme.palette.neutralPrimary,
-                      margin: 8,
-                    },
-                  }}
-                />
-              )}
-              <ExecStage value={inputStage} />
-            </>
-          ))}
-        </>
-      ) : null}
-    </>
-  )
-}
+import { ExecStage } from './ExecStage'
 
 export function SystemProfileCard(props: { value: SystemProfileDoc }) {
   const theme = getTheme()
@@ -235,6 +99,7 @@ export function SystemProfileCard(props: { value: SystemProfileDoc }) {
             display: 'flex',
             flexDirection: 'row-reverse',
             justifyContent: 'flex-end',
+            alignItems: 'center',
           }}>
           {props.value.execStats ? (
             <ExecStage value={props.value.execStats} />
