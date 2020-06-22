9f07f5c30fdcfa15bc18fba0dc8e5d719af37dd7
Zehua Ren <renzholy@gmail.com>
2020-06-22 15:30:00 +0800

    ExecStage

---
+++ src/components/ExecStage.tsx
@@ -0,0 +1,142 @@
+import React, { useEffect, useState, useMemo, useCallback } from 'react'
+import {
+  getTheme,
+  Icon,
+  CompoundButton,
+  HoverCard,
+  HoverCardType,
+} from '@fluentui/react'
+import _ from 'lodash'
+import bytes from 'bytes'
+
+import { ExecStats } from '@/types'
+import { stringify } from '@/utils/mongo-shell-data'
+import { colorize } from '@/utils/editor'
+import { useDarkMode } from '@/utils/theme'
+import { Number } from '@/utils/formatter'
+
+export function ExecStage(props: { value: ExecStats }) {
+  const theme = getTheme()
+  const isDarkMode = useDarkMode()
+  const str = useMemo(
+    () => stringify(_.omit(props.value, ['inputStage', 'inputStages']), 2),
+    [props.value],
+  )
+  const [html, setHtml] = useState(str)
+  useEffect(() => {
+    colorize(str, isDarkMode).then(setHtml)
+  }, [str, isDarkMode])
+  const onRenderPlainCard = useCallback(() => {
+    return (
+      <div
+        style={{
+          paddingLeft: 10,
+          paddingRight: 10,
+          maxWidth: 500,
+          maxHeight: 500,
+          overflowY: 'scroll',
+          backgroundColor: theme.palette.neutralLighterAlt,
+        }}>
+        <pre
+          style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    )
+  }, [html])
+
+  return (
+    <>
+      <HoverCard
+        type={HoverCardType.plain}
+        plainCardProps={{ onRenderPlainCard }}
+        styles={{
+          host: {
+            display: 'inherit',
+            cursor: 'pointer',
+          },
+        }}
+        instantOpenOnClick={true}>
+        <CompoundButton
+          styles={{
+            description: {
+              whiteSpace: 'pre-wrap',
+              lineHeight: '1.2em',
+            },
+            root: {
+              paddingTop: 10,
+              paddingBottom: 10,
+              minHeight: 'unset',
+              height: 'fit-content',
+            },
+          }}
+          secondaryText={_.compact([
+            `${Number.format(
+              props.value.executionTimeMillisEstimate -
+                (props.value.inputStage?.executionTimeMillisEstimate || 0),
+            )} ms`,
+            props.value.docsExamined === undefined
+              ? undefined
+              : `${Number.format(props.value.docsExamined)} docs examined`,
+            props.value.keysExamined === undefined
+              ? undefined
+              : `${Number.format(props.value.keysExamined)} keys examined`,
+            props.value.nMatched === undefined
+              ? undefined
+              : `${Number.format(props.value.nMatched)} matched`,
+            props.value.memUsage === undefined
+              ? undefined
+              : `${bytes(props.value.memUsage, {
+                  unitSeparator: ' ',
+                }).toLocaleLowerCase()} mem used`,
+            `${Number.format(props.value.nReturned)} returned`,
+          ]).join('\n')}>
+          {props.value.stage}
+        </CompoundButton>
+      </HoverCard>
+      {props.value.inputStage ? (
+        <>
+          <Icon
+            iconName="Forward"
+            styles={{
+              root: {
+                color: theme.palette.neutralPrimary,
+                margin: 8,
+              },
+            }}
+          />
+          <ExecStage value={props.value.inputStage} />
+        </>
+      ) : null}
+      {props.value.inputStages ? (
+        <>
+          <Icon
+            iconName="Forward"
+            styles={{
+              root: {
+                color: theme.palette.neutralPrimary,
+                margin: 8,
+              },
+            }}
+          />
+          {props.value.inputStages.map((inputStage, index) => (
+            <>
+              {index === 0 ? null : (
+                <Icon
+                  iconName="Pause"
+                  styles={{
+                    root: {
+                      color: theme.palette.neutralPrimary,
+                      margin: 8,
+                    },
+                  }}
+                />
+              )}
+              <ExecStage value={inputStage} />
+            </>
+          ))}
+        </>
+      ) : null}
+    </>
+  )
+}
