80313766da1f5ea4e7825ccdc78c36527bccdc4e
Zehua Ren <renzholy@gmail.com>
2020-06-22 16:13:44 +0800

    Update profiling.tsx

--- src/pages/profiling.tsx
+++ src/pages/profiling.tsx
@@ -10,12 +10,6 @@ import { LargeMessage } from '@/components/LargeMessage'
 import { SystemProfilePagination } from '@/components/SystemProfilePagination'
 import { SystemProfileCard } from '@/components/SystemProfileCard'
 
-enum ProfilingLevel {
-  OFF = 0,
-  SLOW = 1,
-  ALL = 2,
-}
-
 export default () => {
   const { database, collection } = useSelector((state) => state.root)
   const { filter, skip, limit } = useSelector((state) => state.docs)
@@ -24,9 +18,6 @@ export default () => {
       profile: -1,
     }),
   )
-  const [profilingLevel, setProfilingLevel] = useState<ProfilingLevel>(
-    ProfilingLevel.OFF,
-  )
   const [slowms, setSlowms] = useState(0)
   const dispatch = useDispatch()
   const [sampleRate, setSampleRate] = useState(0)
@@ -34,7 +25,6 @@ export default () => {
     if (!profile) {
       return
     }
-    setProfilingLevel(profile.was)
     setSlowms(profile.slowms)
     setSampleRate(profile.sampleRate)
   }, [profile])
@@ -76,7 +66,7 @@ export default () => {
       setLoading(true)
       try {
         await runCommand(database, {
-          profile: 0,
+          profile: 1,
           slowms: _slowms,
           sampleRate: { $numberDouble: _sampleRate.toString() },
         })
@@ -88,73 +78,58 @@ export default () => {
     [database],
   )
 
+  if (!database || !collection) {
+    return <LargeMessage iconName="Back" title="Select collection" />
+  }
   return (
     <>
       <Stack
         horizontal={true}
         tokens={{ childrenGap: 10, padding: 10 }}
         styles={{ root: { height: 52, alignItems: 'center' } }}>
-        <Label disabled={loading}>Profiling Level:</Label>
-        <Dropdown
+        <SpinButton
           disabled={loading}
-          selectedKey={profilingLevel}
-          styles={{ root: { width: 80 } }}
-          onChange={(_ev, option) => {
-            setProfilingLevel(option?.key as ProfilingLevel)
+          label="Slow Ms:"
+          styles={{
+            spinButtonWrapper: { width: 80 },
+            label: { marginLeft: 10 },
+            root: { width: 'fit-content', marginRight: 10 },
           }}
-          options={[
-            { key: ProfilingLevel.OFF, text: 'Off' },
-            { key: ProfilingLevel.SLOW, text: 'Slow' },
-            { key: ProfilingLevel.ALL, text: 'All' },
-          ]}
+          value={slowms.toString()}
+          step={10}
+          onBlur={(ev) => {
+            const _slowms = Math.max(parseInt(ev.target.value, 10), 0)
+            setSlowms(_slowms)
+            handleSetProfile(_slowms, sampleRate)
+          }}
+          onIncrement={(value) => {
+            const _slowms = Math.max(parseInt(value, 10) + 10, 0)
+            setSlowms(_slowms)
+            handleSetProfile(_slowms, sampleRate)
+          }}
+          onDecrement={(value) => {
+            const _slowms = Math.max(parseInt(value, 10) - 10, 0)
+            setSlowms(_slowms)
+            handleSetProfile(_slowms, sampleRate)
+          }}
         />
-        {profilingLevel === ProfilingLevel.SLOW ? (
-          <>
-            <SpinButton
-              disabled={loading}
-              label="Slow Ms:"
-              styles={{
-                spinButtonWrapper: { width: 80 },
-                label: { marginLeft: 10 },
-                root: { width: 'fit-content', marginRight: 10 },
-              }}
-              value={slowms.toString()}
-              step={10}
-              onBlur={(ev) => {
-                const _slowms = Math.max(parseInt(ev.target.value, 10), 0)
-                setSlowms(_slowms)
-                handleSetProfile(_slowms, sampleRate)
-              }}
-              onIncrement={(value) => {
-                const _slowms = Math.max(parseInt(value, 10) + 10, 0)
-                setSlowms(_slowms)
-                handleSetProfile(_slowms, sampleRate)
-              }}
-              onDecrement={(value) => {
-                const _slowms = Math.max(parseInt(value, 10) - 10, 0)
-                setSlowms(_slowms)
-                handleSetProfile(_slowms, sampleRate)
-              }}
-            />
-            <Label disabled={loading}>Sample Rate:</Label>
-            <Slider
-              disabled={loading}
-              styles={{
-                slideBox: { width: 100 },
-              }}
-              min={0}
-              max={1}
-              step={0.01}
-              valueFormat={(value) => `${Math.round(value * 100)}%`}
-              value={sampleRate}
-              onChange={setSampleRate}
-              onChanged={(_ev, value) => {
-                setSampleRate(value)
-                handleSetProfile(slowms, value)
-              }}
-            />
-          </>
-        ) : null}
+        <Label disabled={loading}>Sample Rate:</Label>
+        <Slider
+          disabled={loading}
+          styles={{
+            slideBox: { width: 100 },
+          }}
+          min={0}
+          max={1}
+          step={0.01}
+          valueFormat={(value) => `${Math.round(value * 100)}%`}
+          value={sampleRate}
+          onChange={setSampleRate}
+          onChanged={(_ev, value) => {
+            setSampleRate(value)
+            handleSetProfile(slowms, value)
+          }}
+        />
         <Stack.Item grow={true}>
           <div />
         </Stack.Item>
@@ -171,9 +146,7 @@ export default () => {
             maxWidth: '100%',
           },
         }}>
-        {!database || !collection ? (
-          <LargeMessage iconName="Back" title="Select collection" />
-        ) : error ? (
+        {error ? (
           <LargeMessage
             iconName="Error"
             title="Error"
