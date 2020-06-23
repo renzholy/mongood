efb9684feb4c443f7a8ae1f1fafff89dd8e335c2
RenzHoly <renzholy@gmail.com>
2020-06-23 22:18:02 +0800

    Update main.go

--- go/main.go
+++ go/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/gobuffalo/packr"
 	"go.mongodb.org/mongo-driver/bson"
@@ -14,12 +15,13 @@ import (
 )
 
 var (
-	ctx    context.Context
-	client *mongo.Client
+	ctx     context.Context
+	clients = sync.Map{}
 )
 
 func runCommand(w http.ResponseWriter, r *http.Request) {
 	type Request struct {
+		URI      string
 		Database string
 		Command  string
 	}
@@ -29,10 +31,16 @@ func runCommand(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	log.Println(request.Database, request.Command)
 	var command interface{}
 	err = bson.UnmarshalExtJSON([]byte(request.Command), true, &command)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	client, err := create(request.URI)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	raw, err := client.Database(request.Database).RunCommand(ctx, command).DecodeBytes()
@@ -44,22 +52,39 @@ func runCommand(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(raw.String()))
 }
 
-func main() {
-	var err error
-
-	// connect mongodb
-	url := os.Getenv("MONGO_URL")
-	if url == "" {
-		url = "mongodb://localhost:27017"
+func create(uri string) (*mongo.Client, error) {
+	cached, ok := clients.Load(uri)
+	if ok && cached != nil {
+		return cached.(*mongo.Client), nil
 	}
-	client, err = mongo.NewClient(options.Client().ApplyURI(url))
+	if uri == "" {
+		if os.Getenv("MONGO_URL") != "" {
+			uri = os.Getenv("MONGO_URL")
+		} else {
+			uri = "mongodb://localhost:27017"
+		}
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
-		log.Fatal(err)
-		return
+		return nil, err
 	}
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	clients.Store(uri, client)
+	return client, nil
+}
+
+func destory() {
+	clients.Range(func(k, v interface{}) bool {
+		v.(*mongo.Client).Disconnect(ctx)
+		return true
+	})
+}
+
+func main() {
 	ctx = context.Background()
-	client.Connect(ctx)
-	defer client.Disconnect(ctx)
 
 	// serve root dir
 	root := os.Getenv("ROOT")
@@ -73,4 +98,6 @@ func main() {
 
 	// start service
 	startService()
+
+	defer destory()
 }
