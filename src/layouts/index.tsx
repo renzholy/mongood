7e349ee49701aa29ce5203846d2e7f251bcf6ab8
Zehua Ren <renzholy@gmail.com>
2020-06-23 11:31:47 +0800

    Update index.tsx

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { RouteComponentProps } from 'react-router'
-import { initializeIcons, loadTheme, getTheme, ITheme } from '@fluentui/react'
+import { initializeIcons, loadTheme, ITheme } from '@fluentui/react'
 import { Provider } from 'react-redux'
 import { ControlledEditor } from '@monaco-editor/react'
 
@@ -15,58 +15,59 @@ export default (props: RouteComponentProps & { children: React.ReactNode }) => {
   const isDarkMode = useDarkMode()
   const [theme, setTheme] = useState<ITheme>()
   useEffect(() => {
-    loadTheme({
-      palette: isDarkMode
-        ? {
-            themePrimary: '#258ede',
-            themeLighterAlt: '#020609',
-            themeLighter: '#061723',
-            themeLight: '#0b2b43',
-            themeTertiary: '#175585',
-            themeSecondary: '#217dc3',
-            themeDarkAlt: '#3998e1',
-            themeDark: '#55a7e6',
-            themeDarker: '#7fbdec',
-            neutralLighterAlt: '#3c3c3c',
-            neutralLighter: '#444444',
-            neutralLight: '#515151',
-            neutralQuaternaryAlt: '#595959',
-            neutralQuaternary: '#5f5f5f',
-            neutralTertiaryAlt: '#7a7a7a',
-            neutralTertiary: '#c8c8c8',
-            neutralSecondary: '#d0d0d0',
-            neutralPrimaryAlt: '#dadada',
-            neutralPrimary: '#ffffff',
-            neutralDark: '#f4f4f4',
-            black: '#f8f8f8',
-            white: '#333333',
-          }
-        : {
-            themePrimary: '#0078d4',
-            themeLighterAlt: '#eff6fc',
-            themeLighter: '#deecf9',
-            themeLight: '#c7e0f4',
-            themeTertiary: '#71afe5',
-            themeSecondary: '#2b88d8',
-            themeDarkAlt: '#106ebe',
-            themeDark: '#005a9e',
-            themeDarker: '#004578',
-            neutralLighterAlt: '#f8f8f8',
-            neutralLighter: '#f4f4f4',
-            neutralLight: '#eaeaea',
-            neutralQuaternaryAlt: '#dadada',
-            neutralQuaternary: '#d0d0d0',
-            neutralTertiaryAlt: '#c8c8c8',
-            neutralTertiary: '#c2c2c2',
-            neutralSecondary: '#858585',
-            neutralPrimaryAlt: '#4b4b4b',
-            neutralPrimary: '#333333',
-            neutralDark: '#272727',
-            black: '#1d1d1d',
-            white: '#ffffff',
-          },
-    })
-    setTheme(getTheme())
+    setTheme(
+      loadTheme({
+        palette: isDarkMode
+          ? {
+              themePrimary: '#258ede',
+              themeLighterAlt: '#020609',
+              themeLighter: '#061723',
+              themeLight: '#0b2b43',
+              themeTertiary: '#175585',
+              themeSecondary: '#217dc3',
+              themeDarkAlt: '#3998e1',
+              themeDark: '#55a7e6',
+              themeDarker: '#7fbdec',
+              neutralLighterAlt: '#3c3c3c',
+              neutralLighter: '#444444',
+              neutralLight: '#515151',
+              neutralQuaternaryAlt: '#595959',
+              neutralQuaternary: '#5f5f5f',
+              neutralTertiaryAlt: '#7a7a7a',
+              neutralTertiary: '#c8c8c8',
+              neutralSecondary: '#d0d0d0',
+              neutralPrimaryAlt: '#dadada',
+              neutralPrimary: '#ffffff',
+              neutralDark: '#f4f4f4',
+              black: '#f8f8f8',
+              white: '#333333',
+            }
+          : {
+              themePrimary: '#0078d4',
+              themeLighterAlt: '#eff6fc',
+              themeLighter: '#deecf9',
+              themeLight: '#c7e0f4',
+              themeTertiary: '#71afe5',
+              themeSecondary: '#2b88d8',
+              themeDarkAlt: '#106ebe',
+              themeDark: '#005a9e',
+              themeDarker: '#004578',
+              neutralLighterAlt: '#f8f8f8',
+              neutralLighter: '#f4f4f4',
+              neutralLight: '#eaeaea',
+              neutralQuaternaryAlt: '#dadada',
+              neutralQuaternary: '#d0d0d0',
+              neutralTertiaryAlt: '#c8c8c8',
+              neutralTertiary: '#c2c2c2',
+              neutralSecondary: '#858585',
+              neutralPrimaryAlt: '#4b4b4b',
+              neutralPrimary: '#333333',
+              neutralDark: '#272727',
+              black: '#1d1d1d',
+              white: '#ffffff',
+            },
+      }),
+    )
   }, [isDarkMode])
 
   return (
