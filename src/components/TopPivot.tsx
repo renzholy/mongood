7fd37809b73855f162df5579c91f290ba93216b4
RenzHoly <renzholy@gmail.com>
2020-06-23 23:05:30 +0800

    Update TopPivot.tsx

--- src/components/TopPivot.tsx
+++ src/components/TopPivot.tsx
@@ -1,30 +1,38 @@
 import React from 'react'
-import { Pivot, PivotItem, getTheme } from '@fluentui/react'
+import { Pivot, PivotItem, getTheme, IconButton } from '@fluentui/react'
 import { useHistory } from 'umi'
+
+import { listConnections } from '@/utils/fetcher'
 
 export function TopPivot() {
   const history = useHistory()
   const theme = getTheme()
+  listConnections()
 
   return (
-    <Pivot
-      selectedKey={history.location.pathname}
-      onLinkClick={(link) => {
-        history.push(link?.props.itemKey || '/')
-      }}
-      styles={{
-        root: {
-          paddingLeft: 8,
-          backgroundColor: theme.palette.neutralLight,
-        },
+    <div
+      style={{
+        backgroundColor: theme.palette.neutralLight,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingLeft: 8,
+        paddingRight: 8,
       }}>
-      <PivotItem headerText="Stats" itemKey="/stats" />
-      <PivotItem headerText="Documents" itemKey="/docs" />
-      <PivotItem headerText="Indexes" itemKey="/indexes" />
-      <PivotItem headerText="Schema" itemKey="/schema" />
-      <PivotItem headerText="Operations" itemKey="/ops" />
-      <PivotItem headerText="Profiling" itemKey="/profiling" />
-      <PivotItem headerText="Users" itemKey="/users" />
-    </Pivot>
+      <Pivot
+        selectedKey={history.location.pathname}
+        onLinkClick={(link) => {
+          history.push(link?.props.itemKey || '/')
+        }}>
+        <PivotItem headerText="Stats" itemKey="/stats" />
+        <PivotItem headerText="Documents" itemKey="/docs" />
+        <PivotItem headerText="Indexes" itemKey="/indexes" />
+        <PivotItem headerText="Schema" itemKey="/schema" />
+        <PivotItem headerText="Operations" itemKey="/ops" />
+        <PivotItem headerText="Profiling" itemKey="/profiling" />
+        <PivotItem headerText="Users" itemKey="/users" />
+      </Pivot>
+      <IconButton iconProps={{ iconName: 'Database' }} />
+    </div>
   )
 }
