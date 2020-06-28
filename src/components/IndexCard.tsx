a65ad953f5150e5ef232bd8eda613598bf628a4e
RenzHoly <renzholy@gmail.com>
2020-06-28 21:19:27 +0800

    Revert "Update IndexCard.tsx"
    This reverts commit 0b7874a3beccd6f232c048f8cf4f8b68a6f15fd0.

--- src/components/IndexCard.tsx
+++ src/components/IndexCard.tsx
@@ -11,7 +11,6 @@ import {
   Stack,
   HoverCard,
   HoverCardType,
-  ContextualMenu,
 } from '@fluentui/react'
 import _ from 'lodash'
 import bytes from 'bytes'
@@ -198,16 +197,9 @@ export function IndexCard(props: {
     })
     props.onDrop()
   }, [database, collection, props.value])
-  const [target, setTarget] = useState<MouseEvent>()
-  const [isMenuHidden, setIsMenuHidden] = useState(true)
 
   return (
     <Card
-      onContextMenu={(ev) => {
-        setTarget(ev.nativeEvent)
-        setIsMenuHidden(false)
-        ev.preventDefault()
-      }}
       onDoubleClick={() => {
         setIsOpen(true)
       }}
@@ -236,23 +228,6 @@ export function IndexCard(props: {
             footer={
               <ActionButton text="Drop" danger={true} onClick={handleDrop} />
             }
-          />
-          <ContextualMenu
-            target={target}
-            hidden={isMenuHidden}
-            onDismiss={() => {
-              setIsMenuHidden(true)
-            }}
-            items={[
-              {
-                key: '0',
-                text: 'View',
-                onClick() {
-                  setIsMenuHidden(true)
-                  setIsOpen(true)
-                },
-              },
-            ]}
           />
           <Text
             variant="xLarge"
