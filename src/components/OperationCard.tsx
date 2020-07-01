9fbb6a3ea3907cfc953a03be3810c5e07d1d3c15
RenzHoly <renzholy@gmail.com>
2020-07-01 23:56:56 +0800

    kill op

--- src/components/OperationCard.tsx
+++ src/components/OperationCard.tsx
@@ -1,24 +1,59 @@
 /* eslint-disable react/no-danger */
 
 import { Card } from '@uifabric/react-cards'
-import { Text, getTheme, ContextualMenu } from '@fluentui/react'
-import React, { useState, useEffect, useMemo } from 'react'
+import {
+  Text,
+  getTheme,
+  ContextualMenu,
+  Dialog,
+  DialogType,
+  DialogFooter,
+  DefaultButton,
+} from '@fluentui/react'
+import React, { useState, useEffect, useMemo, useCallback } from 'react'
 import _ from 'lodash'
+import { useSelector } from 'react-redux'
+import { runCommand } from '@/utils/fetcher'
 
 import { Number } from '@/utils/formatter'
 import { Operation } from '@/types'
 import { useColorize } from '@/hooks/use-colorize'
 import { stringify } from '@/utils/mongo-shell-data'
 import { EditorModal } from './EditorModal'
+import { ActionButton } from './ActionButton'
 
 export function OperationCard(props: {
   value: Operation
   onView(isOpen: boolean): void
+  onKill(): void
 }) {
+  const { connection } = useSelector((state) => state.root)
   const theme = getTheme()
   const [isOpen, setIsOpen] = useState(false)
   const [target, setTarget] = useState<MouseEvent>()
   const [isMenuHidden, setIsMenuHidden] = useState(true)
+  const [isSucceed, setIsSucceed] = useState<boolean>()
+  const [isKilling, setIsKilling] = useState(false)
+  const [hidden, setHidden] = useState(true)
+  useEffect(() => {
+    setIsSucceed(undefined)
+  }, [target])
+  const handleKill = useCallback(async () => {
+    try {
+      setIsKilling(true)
+      await runCommand(connection, 'admin', {
+        killOp: 1,
+        op: props.value.opid,
+      })
+      setIsSucceed(true)
+      setHidden(true)
+      props.onKill()
+    } catch {
+      setIsSucceed(false)
+    } finally {
+      setIsKilling(false)
+    }
+  }, [connection, props.value.opid])
   useEffect(() => {
     props.onView(isOpen)
   }, [isOpen])
@@ -66,13 +101,14 @@ export function OperationCard(props: {
       }}>
       <Card.Item>
         <EditorModal
-          title="View Current Operation"
+          title="View Operation"
           readOnly={true}
           value={props.value}
           isOpen={isOpen}
           onDismiss={() => {
             setIsOpen(false)
           }}
+          footer={<ActionButton text="kill" onClick={handleKill} />}
         />
         <ContextualMenu
           target={target}
@@ -90,8 +126,57 @@ export function OperationCard(props: {
                 setIsOpen(true)
               },
             },
+            {
+              key: '1',
+              text: 'Kill',
+              iconProps: {
+                iconName: 'StatusCircleBlock',
+                styles: { root: { color: theme.palette.red } },
+              },
+              onClick() {
+                setHidden(false)
+              },
+            },
           ]}
         />
+        <Dialog
+          hidden={hidden}
+          dialogContentProps={{
+            type: DialogType.normal,
+            title: `Kill Operation ${props.value.opid}`,
+            showCloseButton: true,
+            onDismiss() {
+              setHidden(true)
+            },
+          }}
+          modalProps={{
+            styles: {
+              main: {
+                minHeight: 0,
+                borderTop: `4px solid ${theme.palette.red}`,
+                backgroundColor: theme.palette.neutralLighterAlt,
+              },
+            },
+            onDismiss() {
+              setHidden(true)
+            },
+          }}>
+          <DialogFooter>
+            <DefaultButton
+              disabled={isKilling}
+              iconProps={
+                {
+                  true: { iconName: 'CheckMark' },
+                  false: { iconName: 'Error' },
+                }[String(isSucceed) as 'true' | 'false']
+              }
+              onClick={() => {
+                handleKill()
+              }}
+              text="Kill"
+            />
+          </DialogFooter>
+        </Dialog>
         <Text
           variant="xLarge"
           styles={{ root: { color: theme.palette.neutralPrimary } }}>
