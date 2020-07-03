307b9b43034ab5e918696e1c26207e34ba5c82de
RenzHoly <renzholy@gmail.com>
2020-07-03 23:37:59 +0800

    Update notebook.tsx

--- src/pages/notebook.tsx
+++ src/pages/notebook.tsx
@@ -13,6 +13,7 @@ import { runCommand } from '@/utils/fetcher'
 import { getTheme, Icon } from '@fluentui/react'
 import { stringify } from '@/utils/ejson'
 import { useColorize } from '@/hooks/use-colorize'
+import { Card } from '@uifabric/react-cards'
 
 export default () => {
   const isDarkMode = useDarkMode()
@@ -46,17 +47,33 @@ export default () => {
   }, [connection, database, command])
   const resultStr = useMemo(() => stringify(result, 2), [result])
   const resultHtml = useColorize(resultStr)
+  const [focus, setFocus] = useState(false)
 
   if (!database) {
     return <LargeMessage iconName="Back" title="Select Database" />
   }
   return (
     <>
-      <div
-        style={{
-          height: 200,
-          flexShrink: 0,
-          position: 'relative',
+      <Card
+        tokens={{
+          padding: 10,
+          maxWidth: 'unset',
+          minHeight: 'unset',
+        }}
+        styles={{
+          root: {
+            backgroundColor: isDarkMode ? '#1e1e1e' : '#fffffe',
+            margin: 20,
+            height: 200,
+            flexShrink: 0,
+            position: 'relative',
+          },
+        }}
+        onFocus={() => {
+          setFocus(true)
+        }}
+        onBlur={() => {
+          setFocus(false)
         }}>
         <ControlledEditor
           language="typescript"
@@ -79,6 +96,9 @@ export default () => {
             })
           }}
           options={{
+            lineDecorationsWidth: 0,
+            glyphMargin: false,
+            folding: false,
             lineNumbers: 'off',
             minimap: { enabled: false },
             wordWrap: 'on',
@@ -86,41 +106,44 @@ export default () => {
             scrollbar: { verticalScrollbarSize: 0, horizontalSliderSize: 0 },
           }}
         />
-        <div
-          style={{
-            position: 'absolute',
-            right: 10,
-            bottom: 10,
-            userSelect: 'none',
-          }}>
-          <span
+        {focus ? (
+          <div
             style={{
-              fontSize: 14,
-              color: theme.palette.neutralSecondary,
+              position: 'absolute',
+              right: 10,
+              bottom: 10,
+              userSelect: 'none',
             }}>
-            ⌘
-          </span>
-          <Icon
-            iconName="ReturnKey"
-            styles={{
-              root: {
-                verticalAlign: 'text-bottom',
+            <span
+              style={{
+                fontSize: 14,
                 color: theme.palette.neutralSecondary,
-              },
-            }}
-          />
-        </div>
-      </div>
+              }}>
+              ⌘
+            </span>
+            <Icon
+              iconName="ReturnKey"
+              styles={{
+                root: {
+                  verticalAlign: 'text-bottom',
+                  color: theme.palette.neutralSecondary,
+                },
+              }}
+            />
+          </div>
+        ) : null}
+      </Card>
       <pre
         style={{
           flex: 1,
           margin: 0,
-          padding: 10,
+          padding: 20,
+          paddingTop: 0,
           fontSize: 12,
-          backgroundColor: theme.palette.neutralLighter,
           whiteSpace: 'pre-wrap',
           wordBreak: 'break-all',
           overflow: 'scroll',
+          color: theme.palette.neutralPrimary,
         }}
         dangerouslySetInnerHTML={{ __html: error?.message || resultHtml }}
       />
