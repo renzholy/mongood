a6aab312f991e272448d2e46f1e70caf7e0d5a4c
Zehua Ren <renzholy@gmail.com>
2020-07-09 19:52:05 +0800

    add why-did-you-render

---
+++ src/utils/wdyr.ts
@@ -0,0 +1,12 @@
+/* eslint-disable global-require */
+
+import React from 'react'
+
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render')
+  const ReactRedux = require('react-redux/lib')
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+    trackExtraHooks: [[ReactRedux, 'useSelector']],
+  })
+}
