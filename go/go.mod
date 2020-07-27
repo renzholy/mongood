737e5dc8701c1dbd8f7a69fcf7d5848aeadc15e9
Zehua Ren <renzholy@gmail.com>
2020-07-28 00:44:50 +0800

    use 'lorca' instead of 'wails' (#57)
    * use lorca
    
    * update deps
    
    * configdir
    
    * port 54034
    
    * update

--- go/go.mod
+++ go/go.mod
@@ -5,8 +5,8 @@ go 1.14
 require (
 	github.com/NYTimes/gziphandler v1.1.1
 	github.com/markbates/pkger v0.17.0
-	github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
-	github.com/wailsapp/wails v1.7.1
+	github.com/shibukawa/configdir v0.0.0-20170330084843-e180dbdc8da0
+	github.com/zserge/lorca v0.1.9
 	go.mongodb.org/mongo-driver v1.3.5
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381
 )
--- go/webview.go
+++ go/webview.go
@@ -5,33 +5,31 @@ package main
 import (
 	"log"
 	"net/http"
-	"strconv"
+	"os"
 
-	"github.com/phayes/freeport"
-	"github.com/wailsapp/wails/lib/renderer/webview"
+	"github.com/shibukawa/configdir"
+	"github.com/zserge/lorca"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 func startService() {
-	intPort, err := freeport.GetFreePort()
-	if err != nil {
-		log.Fatal(err)
-		return
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "54034" // 27017 * 2
 	}
-	port := strconv.Itoa(intPort)
 	s := &http.Server{
 		Addr:    ":" + port,
 		Handler: h2c.NewHandler(mux, &http2.Server{}),
 	}
 	go s.ListenAndServe()
-	w := webview.NewWebview(webview.Settings{
-		Title:     "Mongood",
-		URL:       "http://localhost:" + port,
-		Width:     1280,
-		Height:    800,
-		Resizable: true,
-	})
-	w.Run()
-	defer w.Terminate()
+	config := configdir.New("Mongood", "Mongood")
+	dir := config.QueryCacheFolder().Path
+	log.Println(dir)
+	ui, err := lorca.New("http://localhost:"+port, dir, 1280, 800)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
+	<-ui.Done()
 }
