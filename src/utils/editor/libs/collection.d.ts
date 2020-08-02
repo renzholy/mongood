ee20d4bd4fab82ccdbbb13550be9a58b3e40f15d
RenzHoly <renzholy@gmail.com>
2020-08-02 17:46:44 +0800

    Update collection.d.ts

--- src/utils/editor/libs/collection.d.ts
+++ src/utils/editor/libs/collection.d.ts
@@ -1,34 +1,76 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-class Cursor {
-  skip(skip: number): Cursor
-  limit(limit: number): Cursor
-  sort(sorter: object): Cursor
-  hint(hint: object | string): Cursor
-  project(projection: object): Cursor
-  explain(): void
-  toArray(): void
+class Cursor<T> {
+  skip(skip: number): Cursor<T>
+
+  limit(limit: number): Cursor<T>
+
+  sort(sorter: object): Cursor<T>
+
+  hint(hint: object | string): Cursor<T>
+
+  project(projection: object): Cursor<T>
+
+  async explain(): Promise<any>
+
+  async toArray(): Promise<T[]>
 }
 
-class Collection {
-  find(filter: object = {}): Cursor
-  findOne(filter: object = {}): void
-  insertOne(doc: object): void
-  insertMany(docs: object[])
-  updateOne(
+class Collection<T = any> {
+  find(filter: object = {}): Cursor<T>
+
+  async findOne(filter: object = {}): Promise<T | null>
+
+  async insertOne(
+    doc: object,
+  ): Promise<{
+    insertedCount: number
+  }>
+
+  async insertMany(
+    docs: object[],
+  ): Promise<{
+    insertedCount: number
+  }>
+
+  async updateOne(
     filter: object,
     update: object,
     options: { upsert?: boolean } = {},
-  ): void
-  updateMany(
+  ): Promise<{
+    matchedCount: number
+    modifiedCount: number
+    upsertedCount: number
+    upsertedId?: typeof ObjectId
+  }>
+
+  async updateMany(
     filter: object,
     update: object,
     options: { upsert?: boolean } = {},
-  ): void
-  deleteOne(filter: object): void
-  deleteMany(filter: object): void
-  estimatedDocumentCount(): void
-  countDocuments(filter: object = {}): Promise<number>
-  getIndexes(): void
+  ): Promise<{
+    matchedCount: number
+    modifiedCount: number
+    upsertedCount: number
+    upsertedId?: typeof ObjectId
+  }>
+
+  async deleteOne(
+    filter: object,
+  ): Promise<{
+    deletedCount: number
+  }>
+
+  async deleteMany(
+    filter: object,
+  ): Promise<{
+    deletedCount: number
+  }>
+
+  async estimatedDocumentCount(): Promise<number>
+
+  async countDocuments(filter: object = {}): Promise<number>
+
+  async listIndexes(): Promise<any[]>
 }
