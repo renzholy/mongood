012cacd247f8c907e40569f6164dd28044684bc2
Zehua Ren <renzholy@gmail.com>
2020-08-11 17:03:09 +0800

    IndexFeatures

--- src/components/IndexCard.tsx
+++ src/components/IndexCard.tsx
@@ -1,22 +1,15 @@
 /* eslint-disable react/jsx-indent */
 
-import React, { useCallback, useState, useMemo, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { Card } from '@uifabric/react-cards'
-import {
-  Text,
-  getTheme,
-  Icon,
-  Stack,
-  HoverCard,
-  HoverCardType,
-} from '@fluentui/react'
-import { map, compact } from 'lodash'
+import { Text, getTheme, Icon, Stack } from '@fluentui/react'
+import { map } from 'lodash'
 import bytes from 'bytes'
 import type { IndexSpecification, WiredTigerData } from 'mongodb'
 
 import { EditorModal } from './EditorModal'
 import { IndexContextualMenu } from './IndexContextualMenu'
-import { ColorizedData } from './ColorizedData'
+import { IndexFeatures } from './IndexFeatures'
 
 function IndexInfo(props: { value: IndexSpecification }) {
   const theme = getTheme()
@@ -62,50 +55,6 @@ function IndexInfo(props: { value: IndexSpecification }) {
   )
 }
 
-function IndexFeature(props: { value: { text: string; data?: object } }) {
-  const theme = getTheme()
-
-  const onRenderPlainCard = useCallback(() => {
-    return (
-      <div
-        style={{
-          padding: 10,
-          maxWidth: 500,
-          maxHeight: 500,
-          overflowY: 'scroll',
-          backgroundColor: theme.palette.neutralLighter,
-        }}>
-        <ColorizedData value={props.value.data} />
-      </div>
-    )
-  }, [props.value.data, theme.palette.neutralLighter])
-
-  return (
-    <HoverCard
-      key={props.value.text}
-      type={HoverCardType.plain}
-      plainCardProps={{
-        onRenderPlainCard,
-      }}
-      styles={{
-        host: {
-          display: 'inherit',
-          cursor: 'pointer',
-        },
-      }}
-      instantOpenOnClick={true}>
-      <Text
-        styles={{
-          root: {
-            color: theme.palette.themePrimary,
-          },
-        }}>
-        {props.value.text}
-      </Text>
-    </HoverCard>
-  )
-}
-
 export function IndexCard(props: {
   value: IndexSpecification
   onDrop(): void
@@ -114,50 +63,7 @@ export function IndexCard(props: {
   statDetail: WiredTigerData
 }) {
   const theme = getTheme()
-  const features = useMemo(
-    () =>
-      compact([
-        props.value.background ? { text: 'BACKGROUND' } : null,
-        props.value.unique ? { text: 'UNIQUE' } : null,
-        props.value.sparse ? { text: 'SPARSE' } : null,
-        props.value.partialFilterExpression
-          ? {
-              text: 'PARTIAL',
-              data: {
-                partialFilterExpression: props.value.partialFilterExpression,
-              },
-            }
-          : null,
-        'expireAfterSeconds' in props.value
-          ? {
-              text: 'TTL',
-              data: {
-                expireAfterSeconds: props.value.expireAfterSeconds,
-              },
-            }
-          : null,
-        '2dsphereIndexVersion' in props.value
-          ? {
-              text: 'GEOSPATIAL',
-              data: {
-                '2dsphereIndexVersion': props.value['2dsphereIndexVersion'],
-              },
-            }
-          : null,
-        'textIndexVersion' in props.value
-          ? {
-              text: 'TEXT',
-              data: {
-                textIndexVersion: props.value.textIndexVersion,
-                default_language: props.value.default_language,
-                language_override: props.value.language_override,
-                weights: props.value.weights,
-              },
-            }
-          : null,
-      ]),
-    [props.value],
-  )
+
   const [isOpen, setIsOpen] = useState(false)
   const [isDetailOpen, setIsDetailOpen] = useState(false)
   const target = useRef<MouseEvent>()
@@ -249,27 +155,7 @@ export function IndexCard(props: {
         <Card.Item>
           <IndexInfo value={props.value} />
         </Card.Item>
-        {features.length ? (
-          <Card.Item>
-            <Stack horizontal={true} tokens={{ childrenGap: 10 }}>
-              {features.map((feature) =>
-                'data' in feature ? (
-                  <IndexFeature key={feature.text} value={feature} />
-                ) : (
-                  <Text
-                    key={feature.text}
-                    styles={{
-                      root: {
-                        color: theme.palette.neutralSecondary,
-                      },
-                    }}>
-                    {feature.text}
-                  </Text>
-                ),
-              )}
-            </Stack>
-          </Card.Item>
-        ) : null}
+        <IndexFeatures value={props.value} />
       </Card.Section>
     </Card>
   )
