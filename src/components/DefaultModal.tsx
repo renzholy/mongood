ed4ad6588b8af90deb8141547613e4fa629af2c7
Zehua Ren <renzholy@gmail.com>
2020-08-21 11:28:38 +0800

    DefaultModal

---
+++ src/components/DefaultModal.tsx
@@ -0,0 +1,74 @@
+import { Modal, IconButton, getTheme, Text } from '@fluentui/react'
+import React from 'react'
+
+export function DefaultModal(props: {
+  title: string
+  isOpen: boolean
+  onDismiss(): void
+  onDismissed?(): void
+  children: React.ReactNode
+  footer?: React.ReactNode
+}) {
+  const theme = getTheme()
+
+  return (
+    <Modal
+      styles={{
+        scrollableContent: {
+          minWidth: 800,
+          minHeight: 600,
+          width: '80vw',
+          height: '80vh',
+          borderTop: `4px solid ${theme.palette.themePrimary}`,
+          overflow: 'hidden',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: theme.palette.neutralLighterAlt,
+        },
+      }}
+      isOpen={props.isOpen}
+      onDismiss={props.onDismiss}
+      onDismissed={props.onDismissed}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: 10,
+          paddingLeft: 20,
+        }}>
+        <Text
+          variant="xLarge"
+          block={true}
+          styles={{
+            root: {
+              alignItems: 'center',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              overflow: 'hidden',
+            },
+          }}>
+          {props.title}
+        </Text>
+        <IconButton
+          styles={{ root: { marginLeft: 10 } }}
+          iconProps={{ iconName: 'Cancel' }}
+          onClick={props.onDismiss}
+        />
+      </div>
+      {props.children}
+      {props.footer ? (
+        <div
+          style={{
+            height: 32,
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'row-reverse',
+            padding: 10,
+          }}>
+          {props.footer}
+        </div>
+      ) : null}
+    </Modal>
+  )
+}
--- src/components/MongoDataModal.tsx
+++ src/components/MongoDataModal.tsx
@@ -1,18 +1,10 @@
-/* eslint-disable react/self-closing-comp */
-
-import {
-  Modal,
-  IconButton,
-  getTheme,
-  Text,
-  Stack,
-  DefaultButton,
-} from '@fluentui/react'
+import { Stack, DefaultButton } from '@fluentui/react'
 import React, { useState } from 'react'
 import { omit } from 'lodash'
 
 import { MongoData } from '@/types'
 import { ColorizedData } from './ColorizedData'
+import { DefaultModal } from './DefaultModal'
 
 export function MongoDataModal(props: {
   tabs: string[]
@@ -23,101 +15,48 @@ export function MongoDataModal(props: {
   onDismiss(): void
   footer?: React.ReactNode
 }) {
-  const theme = getTheme()
   const [tab, setTab] = useState<string>()
 
   return (
-    <>
-      <Modal
-        styles={{
-          scrollableContent: {
-            minWidth: 800,
-            minHeight: 600,
-            width: '80vw',
-            height: '80vh',
-            borderTop: `4px solid ${theme.palette.themePrimary}`,
-            overflow: 'hidden',
-            display: 'flex',
-            flexDirection: 'column',
-            backgroundColor: theme.palette.neutralLighterAlt,
-          },
-        }}
-        isOpen={props.isOpen}
-        onDismiss={props.onDismiss}>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            padding: 10,
-            paddingLeft: 20,
-          }}>
-          <Text
-            variant="xLarge"
-            block={true}
-            styles={{
-              root: {
-                alignItems: 'center',
-                whiteSpace: 'nowrap',
-                textOverflow: 'ellipsis',
-                overflow: 'hidden',
-              },
-            }}>
-            {props.title}
-          </Text>
-          <IconButton
-            styles={{ root: { marginLeft: 10 } }}
-            iconProps={{ iconName: 'Cancel' }}
-            onClick={props.onDismiss}
-          />
-        </div>
-        <Stack
-          horizontal={true}
-          tokens={{ childrenGap: 10 }}
-          styles={{ root: { marginLeft: 20, marginRight: 20 } }}>
-          {props.tabs.map((t) => (
-            <DefaultButton
-              key={t}
-              text={t}
-              primary={tab === t}
-              onClick={() => {
-                setTab(t)
-              }}
-            />
-          ))}
+    <DefaultModal
+      title={props.title}
+      isOpen={props.isOpen}
+      onDismiss={props.onDismiss}
+      footer={props.footer}>
+      <Stack
+        horizontal={true}
+        tokens={{ childrenGap: 10 }}
+        styles={{ root: { marginLeft: 20, marginRight: 20 } }}>
+        {props.tabs.map((t) => (
           <DefaultButton
-            text="other"
-            primary={tab === undefined}
+            key={t}
+            text={t}
+            primary={tab === t}
             onClick={() => {
-              setTab(undefined)
+              setTab(t)
             }}
           />
-        </Stack>
-        <div style={{ flex: 1, margin: 20, overflow: 'scroll' }}>
-          {(tab && props.onRenderTab?.(tab)) || (
-            <ColorizedData
-              value={
-                tab === undefined
-                  ? omit(props.value, props.tabs)
-                  : props.value[tab]
-              }
-              style={{ marginBottom: 20 }}
-            />
-          )}
-        </div>
-        {props.footer ? (
-          <div
-            style={{
-              height: 32,
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'row-reverse',
-              padding: 10,
-            }}>
-            {props.footer}
-          </div>
-        ) : null}
-      </Modal>
-    </>
+        ))}
+        <DefaultButton
+          text="other"
+          primary={tab === undefined}
+          onClick={() => {
+            setTab(undefined)
+          }}
+        />
+      </Stack>
+      <div style={{ flex: 1, margin: 20, overflow: 'scroll' }}>
+        {(tab && props.onRenderTab?.(tab)) || (
+          <ColorizedData
+            value={
+              tab === undefined
+                ? omit(props.value, props.tabs)
+                : props.value[tab]
+            }
+            style={{ marginBottom: 20 }}
+          />
+        )}
+      </div>
+    </DefaultModal>
   )
 }
