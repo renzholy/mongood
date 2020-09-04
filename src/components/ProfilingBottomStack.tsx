685571dc15d5d5fea158f905465696371fced93d
Zehua Ren <renzholy@gmail.com>
2020-09-04 10:39:45 +0800

    ProfilingBottomStack

---
+++ src/components/ProfilingBottomStack.tsx
@@ -0,0 +1,82 @@
+import { Stack, Label, Dropdown } from '@fluentui/react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { useSelector } from 'react-redux'
+
+import { useCommandProfile } from '@/hooks/use-command'
+import { usePromise } from '@/hooks/use-promise'
+import { runCommand } from '@/utils/fetcher'
+import { generateConnectionWithDirectHost } from '@/utils'
+import { PromiseButton } from './PromiseButton'
+
+enum ProfilingLevel {
+  OFF = 0,
+  SLOW = 1,
+  ALL = 2,
+}
+
+export function ProfilingBottomStack() {
+  const connection = useSelector((state) => state.root.connection)
+  const host = useSelector((state) => state.profiling.host)
+  const profilingConnection = host
+    ? generateConnectionWithDirectHost(host, connection)
+    : connection
+  const database = useSelector((state) => state.root.database)
+  const [level, setLevel] = useState<ProfilingLevel>()
+  const { data: profile, error, revalidate, isValidating } = useCommandProfile()
+  const handleSetProfile = useCallback(
+    async () =>
+      database && level !== undefined
+        ? runCommand(profilingConnection, database, {
+            profile: level,
+          })
+        : undefined,
+    [profilingConnection, database, level],
+  )
+  const promiseSetProfile = usePromise(handleSetProfile)
+  useEffect(() => {
+    if (promiseSetProfile.resolved) {
+      revalidate()
+    }
+  }, [promiseSetProfile.resolved, revalidate])
+  useEffect(() => {
+    if (!profile) {
+      return
+    }
+    setLevel(profile.was)
+  }, [profile])
+
+  return (
+    <Stack
+      horizontal={true}
+      tokens={{ childrenGap: 10, padding: 10 }}
+      styles={{
+        root: { height: 52, alignItems: 'center' },
+      }}>
+      {error ? null : (
+        <>
+          <Label>Level:</Label>
+          <Dropdown
+            selectedKey={level}
+            onChange={(_ev, option) => {
+              setLevel(option?.key as ProfilingLevel)
+            }}
+            styles={{ root: { width: 80 } }}
+            options={[
+              { key: ProfilingLevel.OFF, text: 'Off' },
+              { key: ProfilingLevel.SLOW, text: 'Slow' },
+              { key: ProfilingLevel.ALL, text: 'All' },
+            ]}
+          />
+        </>
+      )}
+      <Stack.Item grow={true}>
+        <div />
+      </Stack.Item>
+      <PromiseButton
+        disabled={profile?.was === level || isValidating || !!error}
+        icon="CheckMark"
+        promise={promiseSetProfile}
+      />
+    </Stack>
+  )
+}
--- src/components/ProfilingControlStack.tsx
+++ src/components/ProfilingControlStack.tsx
@@ -3,55 +3,17 @@ import {
   Label,
   IContextualMenuItem,
   DefaultButton,
-  Dropdown,
 } from '@fluentui/react'
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { actions } from '@/stores'
-import { useCommandProfile, useCommandIsMaster } from '@/hooks/use-command'
-import { usePromise } from '@/hooks/use-promise'
-import { runCommand } from '@/utils/fetcher'
-import { generateConnectionWithDirectHost } from '@/utils'
+import { useCommandIsMaster } from '@/hooks/use-command'
 import { ProfilingPagination } from './ProfilingPagination'
-import { PromiseButton } from './PromiseButton'
-
-enum ProfilingLevel {
-  OFF = 0,
-  SLOW = 1,
-  ALL = 2,
-}
 
 export function ProfilingControlStack() {
   const connection = useSelector((state) => state.root.connection)
   const host = useSelector((state) => state.profiling.host)
-  const profilingConnection = host
-    ? generateConnectionWithDirectHost(host, connection)
-    : connection
-  const database = useSelector((state) => state.root.database)
-  const [level, setLevel] = useState<ProfilingLevel>()
-  const { data: profile, error, revalidate, isValidating } = useCommandProfile()
-  const handleSetProfile = useCallback(
-    async () =>
-      database && level !== undefined
-        ? runCommand(profilingConnection, database, {
-            profile: level,
-          })
-        : undefined,
-    [profilingConnection, database, level],
-  )
-  const promiseSetProfile = usePromise(handleSetProfile)
-  useEffect(() => {
-    if (promiseSetProfile.resolved) {
-      revalidate()
-    }
-  }, [promiseSetProfile.resolved, revalidate])
-  useEffect(() => {
-    if (!profile) {
-      return
-    }
-    setLevel(profile.was)
-  }, [profile])
   const dispatch = useDispatch()
   const { data: replicaConfig } = useCommandIsMaster()
   const hosts = useMemo<string[]>(() => replicaConfig?.hosts || [], [
@@ -83,50 +45,13 @@ export function ProfilingControlStack() {
       styles={{
         root: { height: 52, alignItems: 'center' },
       }}>
-      {error ? null : (
-        <>
-          <Label>Host:</Label>
-          <DefaultButton
-            menuProps={{
-              items,
-            }}
-            styles={{
-              root: { width: 160, marginRight: 10 },
-              label: {
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-                display: 'block',
-                textAlign: 'start',
-                whiteSpace: 'nowrap',
-              },
-              textContainer: {
-                flex: 1,
-                width: 0,
-              },
-            }}
-            menuIconProps={{ hidden: true }}>
-            {host}
-          </DefaultButton>
-          <Label>Level:</Label>
-          <Dropdown
-            selectedKey={level}
-            onChange={(_ev, option) => {
-              setLevel(option?.key as ProfilingLevel)
-            }}
-            styles={{ root: { width: 80 } }}
-            options={[
-              { key: ProfilingLevel.OFF, text: 'Off' },
-              { key: ProfilingLevel.SLOW, text: 'Slow' },
-              { key: ProfilingLevel.ALL, text: 'All' },
-            ]}
-          />
-        </>
-      )}
-      {profile?.was === level || isValidating || error ? null : (
-        <div>
-          <PromiseButton icon="CheckMark" promise={promiseSetProfile} />
-        </div>
-      )}
+      <Label>Host:</Label>
+      <DefaultButton
+        menuProps={{
+          items,
+        }}>
+        {host}
+      </DefaultButton>
       <Stack.Item grow={true}>
         <div />
       </Stack.Item>
