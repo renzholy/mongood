e22c69c97e134272c8386250863de46dadb8fd86
Found Pan Tiger <renzholy@gmail.com>
2020-09-09 16:10:48 +0800

    fix lint

--- src/hooks/use-promise.ts
+++ src/hooks/use-promise.ts
@@ -1,13 +1,13 @@
 import { useState, useCallback, useEffect } from 'react'
 
-export function usePromise<I extends unknown[], O>(
-  handler: (...input: I) => Promise<O | undefined>,
+export function usePromise<T extends unknown[], O>(
+  handler: (...input: T) => Promise<O | undefined>,
 ) {
   const [resolved, setResolved] = useState<O>()
   const [rejected, setRejected] = useState<Error>()
   const [pending, setPending] = useState(false)
   const call = useCallback(
-    async (...input: I) => {
+    async (...input: T) => {
       try {
         setPending(true)
         setResolved(await handler(...input))
@@ -19,6 +19,7 @@ export function usePromise<I extends unknown[], O>(
         setPending(false)
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [handler],
   )
   const reset = useCallback(() => {
