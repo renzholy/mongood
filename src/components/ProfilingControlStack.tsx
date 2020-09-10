9705cf2126b49031bdd6bc4db3046c69fb38b325
Found Pan Tiger <renzholy@gmail.com>
2020-09-10 14:30:46 +0800

    HostButton

--- src/components/ProfilingControlStack.tsx
+++ src/components/ProfilingControlStack.tsx
@@ -1,45 +1,23 @@
-import {
-  Stack,
-  Label,
-  IContextualMenuItem,
-  DefaultButton,
-} from '@fluentui/react'
-import React, { useEffect, useMemo } from 'react'
+import { Stack, Label } from '@fluentui/react'
+import React, { useEffect, useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { actions } from '@/stores'
-import { useCommandIsMaster } from '@/hooks/use-command'
 import { ProfilingPagination } from './ProfilingPagination'
+import { HostButton } from './pure/HostButton'
 
 export function ProfilingControlStack() {
-  const connection = useSelector((state) => state.root.connection)
   const host = useSelector((state) => state.profiling.host)
   const dispatch = useDispatch()
-  const { data: replicaConfig } = useCommandIsMaster()
-  const hosts = useMemo<string[]>(() => replicaConfig?.hosts || [], [
-    replicaConfig,
-  ])
-  const items = useMemo<IContextualMenuItem[]>(
-    () =>
-      hosts.map((h) => ({
-        key: h,
-        text: h,
-        checked: h === host,
-        canCheck: true,
-        onClick() {
-          dispatch(actions.profiling.setHost(h))
-        },
-      })),
-    [dispatch, host, hosts],
-  )
-  useEffect(() => {
-    if (!host || !hosts.includes(host)) {
-      dispatch(actions.profiling.setHost(hosts[0]))
-    }
-  }, [dispatch, host, hosts, connection])
   useEffect(() => {
     dispatch(actions.profiling.resetPage())
   }, [host, dispatch])
+  const handleSetHost = useCallback(
+    (h: string) => {
+      dispatch(actions.profiling.setHost(h))
+    },
+    [dispatch],
+  )
 
   return (
     <Stack
@@ -48,17 +26,8 @@ export function ProfilingControlStack() {
       styles={{
         root: { height: 52, alignItems: 'center' },
       }}>
-      {items.length === 0 ? null : (
-        <>
-          <Label>Host:</Label>
-          <DefaultButton
-            menuProps={{
-              items,
-            }}>
-            {host}
-          </DefaultButton>
-        </>
-      )}
+      <Label>Host:</Label>
+      <HostButton host={host} setHost={handleSetHost} />
       <Stack.Item grow={true}>
         <div />
       </Stack.Item>
---
+++ src/components/pure/HostButton.tsx
@@ -0,0 +1,45 @@
+import React, { useMemo, useEffect } from 'react'
+import { DefaultButton, IContextualMenuItem, IStyle } from '@fluentui/react'
+
+import { useCommandIsMaster } from '@/hooks/use-command'
+
+export function HostButton(props: {
+  host?: string
+  setHost(host: string): void
+  style?: IStyle
+}) {
+  const { data: replicaConfig } = useCommandIsMaster()
+  const hosts = useMemo<string[]>(() => replicaConfig?.hosts || [], [
+    replicaConfig,
+  ])
+  useEffect(() => {
+    if (!props.host || !hosts.includes(props.host)) {
+      props.setHost(hosts[0])
+    }
+  }, [hosts, props])
+  const items = useMemo<IContextualMenuItem[]>(
+    () =>
+      hosts.map((h) => ({
+        key: h,
+        text: h,
+        checked: h === props.host,
+        canCheck: true,
+        onClick() {
+          props.setHost(h)
+        },
+      })),
+    [props, hosts],
+  )
+
+  return (
+    <DefaultButton
+      disabled={hosts.length === 0}
+      styles={{ root: props.style }}
+      menuProps={{
+        items,
+      }}
+      menuIconProps={{ hidden: true }}>
+      {props.host || 'No Host'}
+    </DefaultButton>
+  )
+}
