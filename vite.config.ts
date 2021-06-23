783cd8c1f317fccd4a78733aaebc826e0837bd66
Zehua Ren <renzholy@gmail.com>
2021-06-23 22:10:26 +0800

    remove some plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import reactJsx from 'vite-react-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      components: '/src/components',
+      hooks: '/src/hooks',
+      layouts: '/src/layouts',
+      pages: '/src/pages',
+      stores: '/src/stores',
+      types: '/src/types',
+      utils: '/src/utils',
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
   },
-  plugins: [tsconfigPaths(), reactJsx(), reactRefresh()],
+  plugins: [reactRefresh()],
 })
