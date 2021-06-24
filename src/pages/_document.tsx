832ad2e526b2d9c07af1519c26e77c17607a3485
Zehua Ren <renzholy@gmail.com>
2021-06-25 00:28:35 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react'
-import { Stylesheet, InjectionMode } from '@uifabric/merge-styles'
-import { resetIds } from '@uifabric/utilities'
-import Document, { DocumentContext } from 'next/document'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+import { InjectionMode, Stylesheet, resetIds } from '@fluentui/react'
 
 const stylesheet = Stylesheet.getInstance()
 
@@ -10,24 +15,35 @@ stylesheet.setConfig({
   namespace: 'server',
 })
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+/**
+ * @see https://github.com/microsoft/fluentui/wiki/Server-side-rendering-and-browserless-testing
+ */
+export default class MyDocument extends Document<{ styleTags: string }> {
+  static async getInitialProps({ renderPage }: DocumentContext) {
     stylesheet.reset()
     resetIds()
 
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      styles: [
-        initialProps.styles,
-        <style
-          key="fluentui-css"
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: stylesheet.getRules(true) }}
-        />,
-      ],
-    }
+    // eslint-disable-next-line react/display-name, react/jsx-props-no-spreading
+    const page = renderPage((App) => (props) => <App {...props} />)
+
+    return { ...page, styleTags: stylesheet.getRules(true) }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style
+            type="text/css"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: this.props.styleTags }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
-
-export default MyDocument
