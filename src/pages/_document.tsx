2793e757a542eb63815dd2d09cfcfe8445419bf0
Found Pan Tiger <renzholy@gmail.com>
2021-10-28 15:54:37 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,27 +5,27 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
-import { InjectionMode, Stylesheet, resetIds } from '@fluentui/react'
+import { Stylesheet, resetIds } from '@fluentui/react'
 
 const stylesheet = Stylesheet.getInstance()
-
-stylesheet.setConfig({
-  injectionMode: InjectionMode.none,
-  namespace: 'server',
-})
 
 /**
  * @see https://github.com/microsoft/fluentui/wiki/Server-side-rendering-and-browserless-testing
  */
-export default class MyDocument extends Document<{ styleTags: string }> {
+export default class MyDocument extends Document<{
+  styleTags: string
+  serializedStylesheet: string
+}> {
   static async getInitialProps({ renderPage }: DocumentContext) {
-    stylesheet.reset()
     resetIds()
 
-    // eslint-disable-next-line react/display-name, react/jsx-props-no-spreading
     const page = renderPage((App) => (props) => <App {...props} />)
 
-    return { ...page, styleTags: stylesheet.getRules(true) }
+    return {
+      ...page,
+      styleTags: stylesheet.getRules(true),
+      serializedStylesheet: stylesheet.serialize(),
+    }
   }
 
   render() {
@@ -34,8 +34,16 @@ export default class MyDocument extends Document<{ styleTags: string }> {
         <Head>
           <style
             type="text/css"
-            // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{ __html: this.props.styleTags }}
+          />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.FabricConfig = window.FabricConfig || {};
+            window.FabricConfig.serializedStylesheet = ${this.props.serializedStylesheet};
+          `,
+            }}
           />
         </Head>
         <body>
