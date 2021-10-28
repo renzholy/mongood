027102c37df7661f6cbe40425eda9906ffa1a03c
Found Pan Tiger <renzholy@gmail.com>
2021-10-28 15:50:22 +0800

    Update indexes.tsx

--- src/pages/indexes.tsx
+++ src/pages/indexes.tsx
@@ -1,5 +1,11 @@
 import { useState, useEffect, useCallback } from 'react'
-import { Stack, IconButton } from '@fluentui/react'
+import {
+  Stack,
+  IconButton,
+  HoverCard,
+  HoverCardType,
+  DefaultButton,
+} from '@fluentui/react'
 import LargeMessage from 'components/pure/large-message'
 import EditorModal from 'components/pure/editor-modal'
 import IndexesList from 'components/indexes-list'
@@ -15,6 +21,7 @@ import Divider from 'components/pure/divider'
 import RefreshButton from 'components/pure/refresh-button'
 import useRouterQuery from 'hooks/use-router-query'
 import { useConnection } from 'hooks/use-connections'
+import MongoDataColorized from 'components/pure/mongo-data-colorized'
 
 export default function Indexes() {
   const [{ conn, database, collection }] = useRouterQuery()
@@ -53,6 +60,35 @@ export default function Indexes() {
   }, [promiseCreate.resolved, mutate])
   const isValidating =
     isValidatingIndex || isValidatingIndexStats || isValidatingCollStats
+  const onRenderPlainCard = useCallback(
+    () => (
+      <div style={{ padding: 10 }}>
+        <p style={{ marginTop: 0 }}>Example:</p>
+        <MongoDataColorized
+          value={{
+            name: 'index_name',
+            key: {
+              a: 1,
+              b: -1,
+            },
+            background: true,
+            unique: true,
+          }}
+        />
+        <DefaultButton
+          text="Reference"
+          iconProps={{ iconName: 'link' }}
+          onClick={() => {
+            window.open(
+              'https://docs.mongodb.com/manual/reference/command/createIndexes/',
+            )
+          }}
+          style={{ marginTop: 10 }}
+        />
+      </div>
+    ),
+    [],
+  )
 
   if (!database || !collection) {
     return <LargeMessage iconName="Back" title="Select Collection" />
@@ -62,7 +98,7 @@ export default function Indexes() {
       <Stack horizontal={true} tokens={{ padding: 10 }}>
         <Stack.Item grow={true}>
           <EditorModal
-            title="Create Index"
+            title={`Create Index for ${database}.${collection}`}
             value={value}
             onChange={setValue}
             isOpen={isOpen}
@@ -82,14 +118,19 @@ export default function Indexes() {
                   primary={true}
                   promise={promiseCreate}
                 />
-                <IconButton
-                  iconProps={{ iconName: 'help' }}
-                  onClick={() => {
-                    window.open(
-                      'https://docs.mongodb.com/manual/reference/method/db.collection.createIndex/#options-for-all-index-types',
-                    )
+                <HoverCard
+                  type={HoverCardType.plain}
+                  plainCardProps={{ onRenderPlainCard }}
+                  styles={{
+                    host: {
+                      display: 'inherit',
+                      cursor: 'pointer',
+                    },
                   }}
-                />
+                  instantOpenOnClick={true}
+                >
+                  <IconButton iconProps={{ iconName: 'help' }} />
+                </HoverCard>
               </>
             )}
           />
